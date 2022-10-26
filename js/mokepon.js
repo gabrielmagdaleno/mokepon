041571c3a0cd16d8999868142ffca0eadadcc477
Gabriel Magdaleno <gmagdaleno1981@gmail.com>
2022-10-26 18:12:43 +0200

    Escuchando eventos con JS (seleccion de mascotas)

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,18 @@
-function selectPlayerPet() {
-    alert("SELECCIONASTE TU MASCOTA");
+function startGame() {
+    let playerPetButton = document.getElementById("pet-button");
+    playerPetButton.addEventListener("click", selectPlayerPet);
 }
 
-let playerPetButton = document.getElementById("pet-button");
-playerPetButton.addEventListener("click", selectPlayerPet);
+function selectPlayerPet() {
+    if (hipodoge.checked) {
+        alert("Seleccionaste Hipodoge");
+    } else if (capipepo.checked) {
+        alert("Seleccionaste Capipepo");
+    } else if (ratigueya.checked) {
+        alert("Seleccionaste Ratigueya");
+    } else {
+        alert("Debes seleccionar una mascota");
+    }
+}
+
+window.addEventListener("load", startGame);
