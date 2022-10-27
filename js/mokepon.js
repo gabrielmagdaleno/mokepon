a6973f644054a4d586dcba422b7a9feec6e86b20
Gabriel Magdaleno <gmagdaleno1981@gmail.com>
2022-10-27 16:04:40 +0200

    Ataques aleatorios del enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,16 @@
+let playerAttack;
+let enemyAttack;
+
 function startGame() {
     let playerPetButton = document.getElementById("pet-button");
     playerPetButton.addEventListener("click", selectPlayerPet);
+
+    let fireButton = document.getElementById("fire-button");
+    fireButton.addEventListener("click", fireAttack);
+    let waterButton = document.getElementById("water-button");
+    waterButton.addEventListener("click", waterAttack);
+    let earthButton = document.getElementById("earth-button");
+    earthButton.addEventListener("click", earthAttack);
 }
 
 function selectPlayerPet() {
@@ -23,15 +33,40 @@ function selectPlayerPet() {
 }
 
 function selectEnemyPet() {
-    let randomAttack = random(1, 3);
+    let randomPet = random(1, 3);
     let spanEnemyPet = document.getElementById("petEnemyName");
 
-    if (randomAttack == 1) {
+    if (randomPet == 1) {
         spanEnemyPet.innerHTML = "Hipodoge";
-    } else if (randomAttack == 2) {
+    } else if (randomPet == 2) {
         spanEnemyPet.innerHTML = "Capipepo";
     } else {
         spanEnemyPet.innerHTML = "Ratigueya";
+    }
+}
+
+function fireAttack() {
+    playerAttack = "FIRE";
+    enemyRandomAttack();
+}
+function waterAttack() {
+    playerAttack = "WATER";
+    enemyRandomAttack();
+}
+function earthAttack() {
+    playerAttack = "EARTH";
+    enemyRandomAttack();
+}
+
+function enemyRandomAttack() {
+    let randomAttack = random(1, 3);
+
+    if (randomAttack == 1) {
+        enemyAttack = "FIRE";
+    } else if (randomAttack == 2) {
+        enemyAttack = "WATER";
+    } else {
+        enemyAttack = "EARTH";
     }
 }
 
